284bf336804b64da3da6bcc911f6b9994fd16b6a
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-11 16:29:34 -0300

    displaying tweets in real-time and emptying data after creating tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,33 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @tweet = Tweet.new
+    @tweets = Tweet.all.order(:created_at)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+
+    respond_to do |format|
+      if @tweet.save
+      format.turbo_stream
+      else
+        format.html do
+          flash[:tweet_errors] = @tweet.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
