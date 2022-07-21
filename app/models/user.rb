0ea1f82b9c38c55d32286191d74e4d5e9462dc25
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-20 21:48:28 -0300

    add validations to make username unique per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
+
+  validates :username, uniqueness: true
 end
