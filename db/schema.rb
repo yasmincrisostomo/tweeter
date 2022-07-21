364894aa08b2f6f9db95e5b8b8193c7b032623e7
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-20 21:43:12 -0300

    add username field to user model and to devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_182322) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_20_225532) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -157,8 +157,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_182322) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
